c2321d3b7d5313627e64aa0f9ae94726b854e1a3
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 10:14:49 +0330

    - Create Import Method In GedcomImport Class

--- FamilyShowLib/GedcomImport.cs
+++ FamilyShowLib/GedcomImport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,40 @@ namespace FamilyShowLib
         // Convert the GEDCOM file to an XML file which is easier 
         // to parse, this contains the GEDCOM info in an XML format.
         private XmlDocument doc;
+
+        public void Import(PeopleCollection peopleCollection, string gedcomFilePath)
+        {
+            // Clear current content.
+            peopleCollection.Clear();
+
+            // First convert the GEDCOM file to an XML file so it's easier to parse,
+            // the temp XML file is deleted when importing is complete.
+            string xmlFilePath = Path.GetTempFileName();
+
+            try
+            {
+                this.people = peopleCollection;
+
+                // Convert the GEDCOM file to a temp XML file.
+                GedcomConverter.ConvertToXml(gedcomFilePath, xmlFilePath, true);
+                doc = new XmlDocument();
+                doc.Load(xmlFilePath);
+
+                // Import data from the temp XML file to the people collection.
+                ImportPeople();
+                ImportFamilies();
+
+                // The collection requires a primary-person, use the first
+                // person added to the collection as the primary-person.
+                if (peopleCollection.Count > 0)
+                    peopleCollection.Current = peopleCollection[0];
+            }
+            finally
+            {
+                // Delete the temp XML file.
+                File.Delete(xmlFilePath);
+            }
+        }
 
 
     }
