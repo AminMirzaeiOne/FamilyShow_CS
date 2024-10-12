3c378e7ea867e16a70f2e74a12cfe9fcd1187fd3
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 17:50:15 +0330

    - Create Based Fileds In Person Class

--- FamilyShowLib/Person.cs
+++ FamilyShowLib/Person.cs
@@ -15,6 +15,31 @@ namespace FamilyShowLib
     [Serializable]
     public class Person : INotifyPropertyChanged, IEquatable<Person>, IDataErrorInfo
     {
+        #region Fields and Constants
 
+        // The constants specific to this class
+        private static class Const
+        {
+            public const string DefaultFirstName = "Unknown";
+        }
+
+        private string id;
+        private string firstName;
+        private string lastName;
+        private string middleName;
+        private string suffix;
+        private string nickName;
+        private string marriedName;
+        private Gender gender;
+        private DateTime? birthDate;
+        private string birthPlace;
+        private DateTime? deathDate;
+        private string deathPlace;
+        private bool isLiving;
+        private PhotoCollection photos;
+        private Story story;
+        private RelationshipCollection relationships;
+
+        #endregion
     }
 }
