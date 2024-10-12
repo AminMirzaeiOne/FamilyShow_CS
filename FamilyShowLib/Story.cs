3d5ddbdeaf69d23a79d6f940803fcc072796bc15
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:35:22 +0330

    - Create PropertyChanged Event In Story Class

--- FamilyShowLib/Story.cs
+++ FamilyShowLib/Story.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -225,6 +226,9 @@ namespace FamilyShowLib
         }
 
         #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
 
     }
 }
