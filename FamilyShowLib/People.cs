c6fb021053b061eee87a2524cd8937914b65c3b8
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 14:09:55 +0330

    - Create PeopleCollection Empty Constractor Class

--- FamilyShowLib/People.cs
+++ FamilyShowLib/People.cs
@@ -392,6 +392,7 @@ namespace FamilyShowLib
     [Serializable]
     public class PeopleCollection : ObservableCollection<Person>, INotifyPropertyChanged
     {
+        public PeopleCollection() { }
 
     }
 }
