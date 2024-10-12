932d722eae48e6b20f3a726d605bf7e659c4f0e6
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 13:49:05 +0330

    - Add CreatePackage Method

--- FamilyShowLib/OPCUtility.cs
+++ FamilyShowLib/OPCUtility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,26 @@ namespace FamilyShowLib
            @"http://schemas.microsoft.com/opc/2006/sample/document";
         private const string ResourceRelationshipType =
             @"http://schemas.microsoft.com/opc/2006/sample/required-resource";
+
+
+        /// <summary>
+        /// Creates a package file containing the content from the specified directory.
+        /// </summary>
+        /// <param name="TargetDirectory">Path to directory containing the content to package</param>
+        public static void CreatePackage(string PackageFileName, string TargetDirectory)
+        {
+            using (Package package = Package.Open(PackageFileName, FileMode.Create))
+            {
+                // Package the contents of the top directory
+                DirectoryInfo mainDirectory = new DirectoryInfo(TargetDirectory);
+                CreatePart(package, mainDirectory, false);
+
+                // Package the contents of the sub-directories
+                foreach (DirectoryInfo di in mainDirectory.GetDirectories())
+                {
+                    CreatePart(package, di, true);
+                }
+            }
+        }
     }
 }
