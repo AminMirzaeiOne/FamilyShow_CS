211d1c66958e4e88f0d434076e42d6b2942d607f
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:05:01 +0330

    - Create FullyQualifiedPath Property In Photo Class

--- FamilyShowLib/Photo.cs
+++ FamilyShowLib/Photo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +36,25 @@ namespace FamilyShowLib
                     relativePath = value;
                     OnPropertyChanged("relativePath");
                 }
+            }
+        }
+
+        /// The fully qualified path to the photo.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "value")]
+        public string FullyQualifiedPath
+        {
+            get
+            {
+                string tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    App.ApplicationFolderName);
+                tempFolder = Path.Combine(tempFolder, App.AppDataFolderName);
+
+                return Path.Combine(tempFolder, relativePath);
+            }
+            set
+            {
+                // This empty setter is needed for serialization.
             }
         }
 
