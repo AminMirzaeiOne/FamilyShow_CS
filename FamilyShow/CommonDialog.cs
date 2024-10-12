a14e53a3f7ed2234c18b51fba4f2e96ce2274e15
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:58:08 +0330

    - Create pinvoke details Region In CommonDilaog Class

--- FamilyShow/CommonDialog.cs
+++ FamilyShow/CommonDialog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -46,5 +47,77 @@ namespace FamilyShow
         }
 
         #endregion
+
+        #region pinvoke details
+
+        private enum OpenFileNameFlags
+        {
+            OFN_READONLY = 0x00000001,
+            OFN_OVERWRITEPROMPT = 0x00000002,
+            OFN_HIDEREADONLY = 0x00000004,
+            OFN_NOCHANGEDIR = 0x00000008,
+            OFN_SHOWHELP = 0x00000010,
+            OFN_ENABLEHOOK = 0x00000020,
+            OFN_ENABLETEMPLATE = 0x00000040,
+            OFN_ENABLETEMPLATEHANDLE = 0x00000080,
+            OFN_NOVALIDATE = 0x00000100,
+            OFN_ALLOWMULTISELECT = 0x00000200,
+            OFN_EXTENSIONDIFFERENT = 0x00000400,
+            OFN_PATHMUSTEXIST = 0x00000800,
+            OFN_FILEMUSTEXIST = 0x00001000,
+            OFN_CREATEPROMPT = 0x00002000,
+            OFN_SHAREAWARE = 0x00004000,
+            OFN_NOREADONLYRETURN = 0x00008000,
+            OFN_NOTESTFILECREATE = 0x00010000,
+            OFN_NONETWORKBUTTON = 0x00020000,
+            OFN_NOLONGNAMES = 0x00040000,
+            OFN_EXPLORER = 0x00080000,
+            OFN_NODEREFERENCELINKS = 0x00100000,
+            OFN_LONGNAMES = 0x00200000,
+            OFN_ENABLEINCLUDENOTIFY = 0x00400000,
+            OFN_ENABLESIZING = 0x00800000,
+            OFN_DONTADDTORECENT = 0x02000000,
+            OFN_FORCESHOWHIDDEN = 0x10000000
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private class OpenFileName
+        {
+            internal int structSize;
+            internal IntPtr owner;
+            internal IntPtr instance;
+            internal string filter;
+            internal string customFilter;
+            internal int maxCustFilter;
+            internal int filterIndex;
+            internal string file;
+            internal int maxFile;
+            internal string fileTitle;
+            internal int maxFileTitle;
+            internal string initialDir;
+            internal string title;
+            internal int flags;
+            internal Int16 fileOffset;
+            internal Int16 fileExtension;
+            internal string defExt;
+            internal IntPtr custData;
+            internal IntPtr hook;
+            internal string templateName;
+        }
+
+        private static class NativeMethods
+        {
+            [DllImport("comdlg32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
+
+            [DllImport("comdlg32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            public static extern bool GetSaveFileName([In, Out] OpenFileName ofn);
+        }
+
+        #endregion
+
+
     }
 }
