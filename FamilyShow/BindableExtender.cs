c44798959c57f51f86a6234e63b8ed4a41c68206
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:50:29 +0330

    - Create GetBindableText Statid Function In BindableExtender Class

--- FamilyShow/BindableExtender.cs
+++ FamilyShow/BindableExtender.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FamilyShow
 {
     public class BindableExtender
     {
-
+        public static string GetBindableText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(BindableTextProperty);
+        }
     }
 }
