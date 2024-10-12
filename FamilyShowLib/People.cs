abe8f2bd022b57bd600403a04b6eb8dcf96ef6b9
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 14:09:29 +0330

    - Create PeopleCollection Class

--- FamilyShowLib/People.cs
+++ FamilyShowLib/People.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -380,6 +382,16 @@ namespace FamilyShowLib
             }
         }
 
+
+    }
+
+
+    /// <summary>
+    /// List of people.
+    /// </summary>
+    [Serializable]
+    public class PeopleCollection : ObservableCollection<Person>, INotifyPropertyChanged
+    {
 
     }
 }
