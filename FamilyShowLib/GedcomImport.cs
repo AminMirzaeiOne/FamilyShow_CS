40fa0a7b2a1de6306beb05a953a3fba7afc61662
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 10:14:17 +0330

    - Create Based Fileds In GedcomImport Class

--- FamilyShowLib/GedcomImport.cs
+++ FamilyShowLib/GedcomImport.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace FamilyShowLib
 {
     public class GedcomImport
     {
+        // The collection to add entries.
+        private PeopleCollection people;
+
+        // Convert the GEDCOM file to an XML file which is easier 
+        // to parse, this contains the GEDCOM info in an XML format.
+        private XmlDocument doc;
+
 
     }
 }
