35e95bfce831928dc5665ce3d161375edc15319c
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:08:56 +0330

    - Create OnPropertyChanged Virtual Method In Photo Class

--- FamilyShowLib/Photo.cs
+++ FamilyShowLib/Photo.cs
@@ -157,6 +157,12 @@ namespace FamilyShowLib
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
 
     }
 }
