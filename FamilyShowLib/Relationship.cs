78a29bde80596a5e9720c0917fdfa67ae3897398
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:22:08 +0330

    - Create RelationshipCollection Class

--- FamilyShowLib/Relationship.cs
+++ FamilyShowLib/Relationship.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -173,4 +174,10 @@ namespace FamilyShowLib
             this.RelationTo = person;
         }
     }
+
+    /// <summary>
+    /// Collection of relationship for a person object
+    /// </summary>
+    [Serializable]
+    public class RelationshipCollection : ObservableCollection<Relationship> { }
 }
