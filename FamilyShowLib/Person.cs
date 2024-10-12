ae30587f6bbc435b68c93e5d426a6e41f386ec86
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 17:49:49 +0330

    - Create Person Class

---
+++ FamilyShowLib/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyShowLib
+{
+    /// <summary>
+    /// Representation for a single serializable Person.
+    /// INotifyPropertyChanged allows properties of the Person class to
+    /// participate as source in data bindings.
+    /// </summary>
+    [Serializable]
+    public class Person : INotifyPropertyChanged, IEquatable<Person>, IDataErrorInfo
+    {
+
+    }
+}
