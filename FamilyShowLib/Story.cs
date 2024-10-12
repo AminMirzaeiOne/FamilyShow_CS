23e37988a07ab563cc80fd1ac5ec2473c074e305
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:35:49 +0330

    - Create OnPropertyChanged Virtual Method In Storty Class

--- FamilyShowLib/Story.cs
+++ FamilyShowLib/Story.cs
@@ -229,6 +229,10 @@ namespace FamilyShowLib
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
