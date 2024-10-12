8a99ad379e581e1e2b9e2773f1bcec15736fff02
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:09:22 +0330

    - Create PhotoCollection Class

--- FamilyShowLib/Photo.cs
+++ FamilyShowLib/Photo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -164,5 +165,13 @@ namespace FamilyShowLib
         }
 
 
+    }
+
+    /// <summary>
+    /// Collection for photos.
+    /// </summary>
+    [Serializable]
+    public class PhotoCollection : ObservableCollection<Photo>
+    {
     }
 }
