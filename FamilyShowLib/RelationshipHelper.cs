adcf5205a881a70bfa91bd6a1f3ea5b3c3cbe03c
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:24:52 +0330

    - Create AddChild Static Method

--- FamilyShowLib/RelationshipHelper.cs
+++ FamilyShowLib/RelationshipHelper.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace FamilyShowLib
 {
+    /// <summary>
+    /// Contains the relationship logic rules for adding people and how they relate to each other.
+    /// </summary>
     public class RelationshipHelper
     {
+        /// <summary>
+        /// Performs the business logic for adding the Child relationship between the person and the child.
+        /// </summary>
+        public static void AddChild(PeopleCollection family, Person person, Person child)
+        {
+            // Add the new child as a sibling to any existing children
+            foreach (Person existingSibling in person.Children)
+            {
+                family.AddSibling(existingSibling, child);
+            }
 
+            switch (person.Spouses.Count)
+            {
+                // Single parent, add the child to the person
+                case 0:
+                    family.AddChild(person, child, ParentChildModifier.Natural);
+                    break;
+
+                // Has existing spouse, add the child to the person's spouse as well.
+                case 1:
+                    family.AddChild(person, child, ParentChildModifier.Natural);
+                    family.AddChild(person.Spouses[0], child, ParentChildModifier.Natural);
+                    break;
+            }
+        }
     }
 }
