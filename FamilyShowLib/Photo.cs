022222cebe2a485ded9085adfa1280f2a73264e2
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:07:32 +0330

    - Create Copy Static Function In Photo Class

--- FamilyShowLib/Photo.cs
+++ FamilyShowLib/Photo.cs
@@ -95,5 +95,51 @@ namespace FamilyShowLib
             return FullyQualifiedPath;
         }
 
+        /// <summary>
+        /// Copies the photo file to the application photos folder. 
+        /// Returns the relative path to the copied photo.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        private static string Copy(string fileName)
+        {
+            // The photo file being copied
+            FileInfo fi = new FileInfo(fileName);
+
+            // Absolute path to the application folder
+            string appLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                App.ApplicationFolderName);
+            appLocation = Path.Combine(appLocation, App.AppDataFolderName);
+
+            // Absolute path to the photos folder
+            string photoLocation = Path.Combine(appLocation, Const.PhotosFolderName);
+
+            // Fully qualified path to the new photo file
+            string photoFullPath = Path.Combine(photoLocation, fi.Name);
+
+            // Relative path to the new photo file
+            string photoRelLocation = Path.Combine(Const.PhotosFolderName, fi.Name);
+
+            // Create the appLocation directory if it doesn't exist
+            if (!Directory.Exists(appLocation))
+                Directory.CreateDirectory(appLocation);
+
+            // Create the photos directory if it doesn't exist
+            if (!Directory.Exists(photoLocation))
+                Directory.CreateDirectory(photoLocation);
+
+            // Copy the photo.
+            try
+            {
+                fi.CopyTo(photoFullPath, true);
+            }
+            catch
+            {
+                // Could not copy the photo. Handle all exceptions 
+                // the same, ignore and continue.
+            }
+
+            return photoRelLocation;
+        }
+
     }
 }
