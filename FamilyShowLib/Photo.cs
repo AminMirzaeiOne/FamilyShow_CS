8c24d0e816f358f8f975c06accad8c061057252b
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-12 18:03:06 +0330

    - Create Const Static Class In Photo Class

--- FamilyShowLib/Photo.cs
+++ FamilyShowLib/Photo.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FamilyShowLib
 {
-    public class Photo
+    /// <summary>
+    /// Simple representation of a serializable photo associated with the Person class
+    /// </summary>
+    [Serializable]
+    public class Photo : INotifyPropertyChanged
     {
-
+        // The constants specific to this class
+        public static class Const
+        {
+            public const string PhotosFolderName = "Images";
+        }
     }
 }
