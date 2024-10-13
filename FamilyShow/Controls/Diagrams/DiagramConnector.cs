e67ea2c45aa739ccda4a7683c8e32c5c45d7c65d
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 10:09:33 +0330

    - Create Based Fileds In DiagramConverter Class

--- FamilyShow/Controls/Diagrams/DiagramConnector.cs
+++ FamilyShow/Controls/Diagrams/DiagramConnector.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
 
 namespace FamilyShow.Controls.Diagrams
 {
@@ -15,5 +17,25 @@ namespace FamilyShow.Controls.Diagrams
             public static double OpacityNormal = 1.0;
             public static double AnimationDuration = 300;
         }
+
+        #region fields
+
+        // The two nodes that are connected.
+        private DiagramConnectorNode start;
+        private DiagramConnectorNode end;
+
+        // Flag, if the connection is currently filtered. The
+        // connection is drawn in a dim-state when filtered.
+        private bool isFiltered;
+
+        // Animation if the filtered state has changed.
+        private DoubleAnimation animation;
+
+        // Pen to draw connector line.
+        private Pen resourcePen;
+
+        #endregion
+
+
     }
 }
