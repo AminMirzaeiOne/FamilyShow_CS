7d024b4613202b2163e1e21bb85ea0e63a7087c6
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 08:22:30 +0330

    - Create and Complete PrimaryAvatarConverter Class

--- FamilyShow/ValueConverters.cs
+++ FamilyShow/ValueConverters.cs
@@ -68,5 +68,29 @@ namespace FamilyShow
         #endregion
     }
 
+    /// <summary>
+    /// This converter is used to show the "*" on the picture when the IsAvatar property is true.
+    /// </summary>
+    public class PrimaryAvatarConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null && (bool)value)
+                return "*";
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException(Properties.Resources.NotImplemented);
+        }
+
+        #endregion
+    }
+
+
 
 }
