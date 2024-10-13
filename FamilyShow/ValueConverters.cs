c0c65e7a29ce8dc47cf7b29db56471bff6f20ff0
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 08:22:54 +0330

    - Create and Complete BoolToVisibilityConverter Class

--- FamilyShow/ValueConverters.cs
+++ FamilyShow/ValueConverters.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace FamilyShow
@@ -90,6 +91,27 @@ namespace FamilyShow
 
         #endregion
     }
+
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((bool)value)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException(Properties.Resources.NotImplemented);
+        }
+
+        #endregion
+    }
+
 
 
 
