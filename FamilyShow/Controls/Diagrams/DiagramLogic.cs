7a9c8fa74bdb9da2286b5e655fac2da820a83ed1
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 10:26:52 +0330

    - Create Base Properties In DiagramLogic Class

--- FamilyShow/Controls/Diagrams/DiagramLogic.cs
+++ FamilyShow/Controls/Diagrams/DiagramLogic.cs
@@ -30,5 +30,95 @@ namespace FamilyShow.Controls.Diagrams
         private double displayYear;
 
         #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Sets the callback that is called when a node is clicked.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        public RoutedEventHandler NodeClickHandler
+        {
+            set { nodeClickHandler = value; }
+        }
+
+        /// <summary>
+        /// Gets the list of people in the family.
+        /// </summary>
+        public PeopleCollection Family
+        {
+            get { return family; }
+        }
+
+        /// <summary>
+        /// Gets the list of connections between nodes.
+        /// </summary>
+        public List<DiagramConnector> Connections
+        {
+            get { return connections; }
+        }
+
+        /// <summary>
+        /// Gets the person lookup list. This includes all of the 
+        /// people and nodes that are displayed in the diagram.
+        /// </summary>
+        public Dictionary<Person, DiagramConnectorNode> PersonLookup
+        {
+            get { return personLookup; }
+        }
+
+        /// <summary>
+        /// Sets the year filter that filters nodes and connectors.
+        /// </summary>
+        public double DisplayYear
+        {
+            set
+            {
+                if (this.displayYear != value)
+                {
+                    this.displayYear = value;
+                    foreach (DiagramConnectorNode connectorNode in personLookup.Values)
+                        connectorNode.Node.DisplayYear = this.displayYear;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the minimum year in all nodes and connectors.
+        /// </summary>
+        public double MinimumYear
+        {
+            get
+            {
+                // Init to current year.
+                double minimumYear = DateTime.Now.Year;
+
+                // Check birth years.
+                foreach (DiagramConnectorNode connectorNode in personLookup.Values)
+                {
+                    DateTime? date = connectorNode.Node.Person.BirthDate;
+                    if (date != null)
+                        minimumYear = Math.Min(minimumYear, date.Value.Year);
+                }
+
+                // Check marriage years.
+                foreach (DiagramConnector connector in connections)
+                {
+                    // Marriage date.
+                    DateTime? date = connector.MarriedDate;
+                    if (date != null)
+                        minimumYear = Math.Min(minimumYear, date.Value.Year);
+
+                    // Previous marriage date.
+                    date = connector.PreviousMarriedDate;
+                    if (date != null)
+                        minimumYear = Math.Min(minimumYear, date.Value.Year);
+                }
+
+                return minimumYear;
+            }
+        }
+
+        #endregion
     }
 }
