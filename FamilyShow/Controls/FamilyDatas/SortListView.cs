15b9b3a885ebad027b0a7b8162230b7539d6141b
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 16:01:17 +0330

    - Create Based Fileds In SortListView Class

--- FamilyShow/Controls/FamilyDatas/SortListView.cs
+++ FamilyShow/Controls/FamilyDatas/SortListView.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace FamilyShow.Controls.FamilyDatas
 {
-    public class SortListView
+    public class SortListView : ListView
     {
+        // The current column that is sorted.
+        private SortListViewColumn sortColumn;
+
+        // The previous column that was sorted.
+        private SortListViewColumn previousSortColumn;
+
+        // The current direction the header is sorted;
+        private ListSortDirection sortDirection;
 
     }
 }
