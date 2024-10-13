8dd8f606a739a24e4b296363cca9d5a5158f461c
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 10:50:45 +0330

    - Create Based Fileds In DiagraRow Class

--- FamilyShow/Controls/Diagrams/DiagramRow.cs
+++ FamilyShow/Controls/Diagrams/DiagramRow.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FamilyShow.Controls.Diagrams
 {
     public class DiagramRow
     {
+        #region fields
 
+        // Space between each group.
+        private double groupSpace = 80;
+
+        // Location of the row, relative to the diagram.
+        private Point location = new Point();
+
+        // List of groups in the row.
+        private List<DiagramGroup> groups = new List<DiagramGroup>();
+
+        #endregion
     }
 }
