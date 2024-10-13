182f9a090383dcfa9e8194c26504c966866c6baf
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 08:23:38 +0330

    - Create and Complete ComposingConverter Class

--- FamilyShow/ValueConverters.cs
+++ FamilyShow/ValueConverters.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -129,7 +130,37 @@ namespace FamilyShow
         #endregion
     }
 
+    public class ComposingConverter : IValueConverter
+    {
+        #region IValueCOnverter Members
 
+        private List<IValueConverter> converters = new List<IValueConverter>();
+
+        public Collection<IValueConverter> Converters
+        {
+            get { return new Collection<IValueConverter>(this.converters); }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            for (int i = 0; i < this.converters.Count; i++)
+            {
+                value = converters[i].Convert(value, targetType, parameter, culture);
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            for (int i = this.converters.Count - 1; i >= 0; i--)
+            {
+                value = converters[i].ConvertBack(value, targetType, parameter, culture);
+            }
+            return value;
+        }
+
+        #endregion
+    }
 
 
 }
