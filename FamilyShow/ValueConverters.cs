57d6789ce2473909f70c2761f2525662c98a69dd
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 08:24:00 +0330

    - Create and Complete ImageConverter Class

--- FamilyShow/ValueConverters.cs
+++ FamilyShow/ValueConverters.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media.Imaging;
 
 namespace FamilyShow
 {
@@ -157,6 +158,35 @@ namespace FamilyShow
                 value = converters[i].ConvertBack(value, targetType, parameter, culture);
             }
             return value;
+        }
+
+        #endregion
+    }
+
+    public class ImageConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                BitmapImage bitmap = new BitmapImage(new Uri(value.ToString()));
+
+                // Use BitmapCacheOption.OnLoad to prevent binding the source holding on to the photo file.
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+
+                return bitmap;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException(Properties.Resources.NotImplemented);
         }
 
         #endregion
