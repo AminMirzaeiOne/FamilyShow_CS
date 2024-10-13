2274339069cf79d9b4d4d3bb0a70248771c05f24
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 10:10:50 +0330

    - Create and Complete DiagramConnectorNode Class

--- FamilyShow/Controls/Diagrams/DiagramConnector.cs
+++ FamilyShow/Controls/Diagrams/DiagramConnector.cs
@@ -5,9 +5,104 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Animation;
 using System.Windows.Media;
+using System.Windows;
 
 namespace FamilyShow.Controls.Diagrams
 {
+    /// <summary>
+    /// One of the nodes in a connection.
+    /// </summary>
+    public class DiagramConnectorNode
+    {
+        #region fields
+
+        // Node location in the diagram.
+        private DiagramRow row;
+        private DiagramGroup group;
+        private DiagramNode node;
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Node for this connection point.
+        /// </summary>
+        public DiagramNode Node
+        {
+            get { return node; }
+        }
+
+        /// <summary>
+        /// Center of the node relative to the diagram.
+        /// </summary>
+        public Point Center
+        {
+            get { return GetPoint(this.node.Center); }
+        }
+
+        /// <summary>
+        /// LeftCenter of the node relative to the diagram.
+        /// </summary>
+        public Point LeftCenter
+        {
+            get { return GetPoint(this.node.LeftCenter); }
+        }
+
+        /// <summary>
+        /// RightCenter of the node relative to the diagram.
+        /// </summary>
+        public Point RightCenter
+        {
+            get { return GetPoint(this.node.RightCenter); }
+        }
+
+        /// <summary>
+        /// TopCenter of the node relative to the diagram.
+        /// </summary>
+        public Point TopCenter
+        {
+            get { return GetPoint(this.node.TopCenter); }
+        }
+
+        /// <summary>
+        /// TopRight of the node relative to the diagram.
+        /// </summary>
+        public Point TopRight
+        {
+            get { return GetPoint(this.node.TopRight); }
+        }
+
+        /// <summary>
+        /// TopLeft of the node relative to the diagram.
+        /// </summary>
+        public Point TopLeft
+        {
+            get { return GetPoint(this.node.TopLeft); }
+        }
+
+        #endregion
+
+        public DiagramConnectorNode(DiagramNode node, DiagramGroup group, DiagramRow row)
+        {
+            this.node = node;
+            this.group = group;
+            this.row = row;
+        }
+
+        /// <summary>
+        /// Return the point shifted by the row and group location.
+        /// </summary>
+        private Point GetPoint(Point point)
+        {
+            point.Offset(
+                this.row.Location.X + this.group.Location.X,
+                this.row.Location.Y + this.group.Location.Y);
+
+            return point;
+        }
+    }
+
     public class DiagramConnector
     {
         private static class Const
