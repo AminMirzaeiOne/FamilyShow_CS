6c7343fbfe50afca9ccbbffe8e880f6b78cf1614
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 10:51:07 +0330

    - Create Properties in DiagramRow Class

--- FamilyShow/Controls/Diagrams/DiagramRow.cs
+++ FamilyShow/Controls/Diagrams/DiagramRow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,47 @@ namespace FamilyShow.Controls.Diagrams
 
         // List of groups in the row.
         private List<DiagramGroup> groups = new List<DiagramGroup>();
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Space between each group.
+        /// </summary>
+        public double GroupSpace
+        {
+            get { return groupSpace; }
+            set { groupSpace = value; }
+        }
+
+        /// <summary>
+        /// Location of the row, relative to the diagram.
+        /// </summary>
+        public Point Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        /// <summary>
+        /// List of groups in the row.
+        /// </summary>
+        public ReadOnlyCollection<DiagramGroup> Groups
+        {
+            get { return new ReadOnlyCollection<DiagramGroup>(groups); }
+        }
+
+        public int NodeCount
+        {
+            get
+            {
+                int count = 0;
+                foreach (DiagramGroup group in groups)
+                    count += group.Nodes.Count;
+                return count;
+            }
+        }
 
         #endregion
     }
