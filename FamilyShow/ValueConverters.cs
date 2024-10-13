20790d8bc7731ca89aa086aabac79b8867f67589
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 08:23:16 +0330

    - Create and Complete NotConverter Class

--- FamilyShow/ValueConverters.cs
+++ FamilyShow/ValueConverters.cs
@@ -112,6 +112,23 @@ namespace FamilyShow
         #endregion
     }
 
+    public class NotConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException(Properties.Resources.NotImplemented);
+        }
+
+        #endregion
+    }
+
 
 
 
