b498e010decd51951537363142ca13159850078d
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 08:21:39 +0330

    - Create and Complete DateFormattingConverter Class

--- FamilyShow/ValueConverters.cs
+++ FamilyShow/ValueConverters.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace FamilyShow
 {
-    public class ValueConverters
+    /// <summary>
+    /// This converter is used to show DateTime in short date format
+    /// </summary>
+    public class DateFormattingConverter : IValueConverter
     {
+        #region IValueConverter Members
 
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+                return ((DateTime)value).ToShortDateString();
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // Ignore empty strings. this will cause the binding to bypass validation.
+            if (string.IsNullOrEmpty((string)value))
+                return Binding.DoNothing;
+
+            string dateString = (string)value;
+
+            // Append first month and day if just the year was entered
+            if (dateString.Length == 4)
+                dateString = "1/1/" + dateString;
+
+            DateTime date;
+            DateTime.TryParse(dateString, out date);
+            return date;
+        }
+
+        #endregion
     }
+
+
 }
