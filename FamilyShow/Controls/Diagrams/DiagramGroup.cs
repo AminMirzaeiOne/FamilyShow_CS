70ea5144ffd1ab2b3cfaa887f88cb0773e106ec0
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 10:20:24 +0330

    - Create Based Fileds In DiagramGroup Class

--- FamilyShow/Controls/Diagrams/DiagramGroup.cs
+++ FamilyShow/Controls/Diagrams/DiagramGroup.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace FamilyShow.Controls.Diagrams
 {
     public class DiagramGroup
     {
+        #region fields
+
+        // Space between each node.
+        private const double NodeSpace = 10;
+
+        // Location of the group, relative to the row.
+        private Point location = new Point();
+
+        // List of nodes in the group.
+        private List<DiagramNode> nodes = new List<DiagramNode>();
+
+        #endregion
 
     }
 }
