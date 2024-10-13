7b8c2d53de5b339f58e96728d5933f22c89e4a39
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 10:44:12 +0330

    - Create Based Fileds In DiagramNode Class

--- FamilyShow/Controls/Diagrams/DiagramNode.cs
+++ FamilyShow/Controls/Diagrams/DiagramNode.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FamilyShowLib;
+using System.Windows;
 
 namespace FamilyShow.Controls.Diagrams
 {
@@ -28,5 +30,28 @@ namespace FamilyShow.Controls.Diagrams
             public static double OpacityNormal = 1.0;
             public static double AnimationDuration = 300;
         }
+
+        #region fields
+
+        // Person object associated with the node.
+        private Person person;
+
+        // Location of the node, relative to its parent group.
+        private Point location = new Point();
+
+        // The type of node.
+        private NodeType type = NodeType.Related;
+
+        // The amount to scale the node. 
+        private double scale = 1;
+
+        // The current display year, this is used for the time filter.
+        private double displayYear = DateTime.Now.Year;
+
+        // Flag, true if this node is currently filtered. This means
+        // its still displayed but in a dim state.
+        private bool isFiltered;
+
+        #endregion
     }
 }
