3907d2ccaa3ec2248267c935451783e8eafd2bf5
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-13 16:01:42 +0330

    - Create OnInitialized Override Method In SortListView Class

--- FamilyShow/Controls/FamilyDatas/SortListView.cs
+++ FamilyShow/Controls/FamilyDatas/SortListView.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace FamilyShow.Controls.FamilyDatas
@@ -18,6 +19,13 @@ namespace FamilyShow.Controls.FamilyDatas
 
         // The current direction the header is sorted;
         private ListSortDirection sortDirection;
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            // Handle the event when a header is clicked.
+            this.AddHandler(GridViewColumnHeader.ClickEvent, new RoutedEventHandler(OnHeaderClicked));
+            base.OnInitialized(e);
+        }
 
     }
 }
