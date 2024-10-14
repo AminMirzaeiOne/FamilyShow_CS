739ebc78a0338c6b69502e95f71ab7971ed258b1
aminmirzaei <aminmirzaei.og@gmail.com>
2024-10-14 10:36:50 +0330

    - Create ParentSet Class

--- FamilyShowLib/ParentSet.cs
+++ FamilyShowLib/ParentSet.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FamilyShowLib
 {
-    public class ParentSetCollection : Collection<ParentSet> { }
+    public class ParentSet
+    {
+    }
 }
